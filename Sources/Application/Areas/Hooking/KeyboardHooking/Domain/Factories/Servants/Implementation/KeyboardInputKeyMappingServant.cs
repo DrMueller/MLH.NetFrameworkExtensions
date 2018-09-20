c9bce88ba416ad202adb9649c22dd302878cd26e
Matthias Mueller <matthiasm@live.de>
2018-09-20 13:25:59 +0200

    WIP

--- Sources/Application/Areas/Hooking/KeyboardHooking/Domain/Factories/Servants/Implementation/KeyboardInputKeyMappingServant.cs
+++ Sources/Application/Areas/Hooking/KeyboardHooking/Domain/Factories/Servants/Implementation/KeyboardInputKeyMappingServant.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 using System.Windows.Forms;
 using Mmu.Mlh.NetFrameworkExtensions.Areas.Hooking.KeyboardHooking.Domain.Models;
 
 namespace Mmu.Mlh.NetFrameworkExtensions.Areas.Hooking.KeyboardHooking.Domain.Factories.Servants.Implementation
 {
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses", Justification = "Instantiated by StrcutureMap")]
     internal class KeyboardInputKeyMappingServant : IKeyboardInputKeyMappingServant
     {
         public KeyboardInputKey MapFromNativeKey(Keys key)
